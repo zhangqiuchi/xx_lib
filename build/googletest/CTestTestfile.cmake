a2c79412d5e302f2c5126d089cb706384d266c86
zhangqiuchi <2078720351@qq.com>
2022-01-14 09:15:51 +0800

    update .gitignore

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/xx/xx_lib/xx_lib/rapidjson/thirdparty/gtest/googletest
-# Build directory: C:/xx/xx_lib/xx_lib/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
